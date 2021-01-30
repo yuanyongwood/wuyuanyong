07187d05cea3fde0e72b0d783dceba09dce450dd
yuanyongwood <yuanyongwood@163.com>
2021-01-30 16:19:03 +0800

    channe操作文件

--- record-learning/src/test/java/com/wood/nio/TestNio.java
+++ record-learning/src/test/java/com/wood/nio/TestNio.java
@@ -3,7 +3,13 @@ package com.wood.nio;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class TestNio {
     /**
@@ -49,9 +55,9 @@ public class TestNio {
         this.printfBufferInfo(buffer.position(), buffer.limit(), buffer.capacity(), "读取10个字节email");
         //记录position当前位置（10）
         buffer.mark();
-        byte[] testRemarkByte=new byte[2];
+        byte[] testRemarkByte = new byte[2];
         //从缓冲区读取2字节数据向testRemarkByte字节数组0的索引位置写入
-        buffer.get(testRemarkByte,0,2);
+        buffer.get(testRemarkByte, 0, 2);
         for (byte b : testRemarkByte) {
             System.out.print((char) b);
         }
@@ -66,5 +72,69 @@ public class TestNio {
 
     void printfBufferInfo(int position, int limit, int capacity, String tips) {
         System.out.printf("--------%s-------%nposition:%d%nlimit:%d%ncapacity:%d%n", tips, position, limit, capacity);
+    }
+
+    /**
+     * 通过操作fileChannel进行读写文件
+     */
+    @Test
+    public void TestFileChannel() throws Exception {
+        //读取原文件路径
+        Path sourcePath = Paths.get("/Users/yuanyongwood/Documents/dog.jpeg");
+        //新增目标文件路径
+        Path targetPath = Paths.get("/Users/yuanyongwood/Documents/target.jpeg");
+        //开启一个读通道
+        FileChannel readChannel = FileChannel.open(sourcePath, StandardOpenOption.READ);
+        //开启一个写通道,CREATE:文件若存在则覆盖，CREATE_NEW:文件若存在则报错
+        FileChannel writeChannel = FileChannel.open(targetPath, StandardOpenOption.CREATE,StandardOpenOption.WRITE);
+        //创建直接缓冲区
+        ByteBuffer buffer=ByteBuffer.allocateDirect(1024);
+        while(readChannel.read(buffer)!=-1){
+            buffer.flip();
+            writeChannel.write(buffer);
+            buffer.clear();
+        }
+        readChannel.close();
+        writeChannel.close();
+    }
+
+    /**
+     * 通过内存映射文件的方式读写文件
+     */
+    @Test
+    public void TestMappedByteBuffer() throws Exception{
+        //读取原文件路径
+        Path sourcePath = Paths.get("/Users/yuanyongwood/Documents/dog.jpeg");
+        //新增目标文件路径
+        Path targetPath = Paths.get("/Users/yuanyongwood/Documents/target.jpeg");
+        FileChannel readChannel = FileChannel.open(sourcePath,StandardOpenOption.READ);
+        FileChannel writeChannel = FileChannel.open(targetPath,StandardOpenOption.WRITE,StandardOpenOption.READ,StandardOpenOption.CREATE);
+        //将readChannel中的数据映射到内存中
+        MappedByteBuffer readMapped = readChannel.map(FileChannel.MapMode.READ_ONLY, 0, readChannel.size());
+        MappedByteBuffer writeMapped = writeChannel.map(FileChannel.MapMode.READ_WRITE, 0, readChannel.size());
+        byte[] temp=new byte[readMapped.limit()];
+        //读写直接操作内存，将readMapped中的数据读取到temp数组中
+        readMapped.get(temp);
+        //读写直接操作内存，将readMapped中的数据写入到writeMapped中
+        writeMapped.put(temp);
+        readChannel.close();
+        writeChannel.close();
+    }
+
+    /**
+     * 通道之间直接传输数据
+     */
+    @Test
+    public void Transform() throws Exception{
+        //读取原文件路径
+        Path sourcePath = Paths.get("/Users/yuanyongwood/Documents/dog.jpeg");
+        //新增目标文件路径
+        Path targetPath = Paths.get("/Users/yuanyongwood/Documents/target.jpeg");
+        FileChannel readChannel = FileChannel.open(sourcePath,StandardOpenOption.READ);
+        FileChannel writeChannel = FileChannel.open(targetPath,StandardOpenOption.WRITE,StandardOpenOption.READ,StandardOpenOption.CREATE);
+        readChannel.transferTo(0,readChannel.size(),writeChannel);
+        writeChannel.transferFrom(readChannel,0,readChannel.size());
+        readChannel.close();
+        writeChannel.close();
     }
 }
