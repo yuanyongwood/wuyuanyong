abd6d3fb793172b9d27db76788a2be98de287f14
yuanyongwood <yuanyongwood@163.com>
2021-01-31 23:49:56 +0800

    selector多路复用开发socketnio服务端

---
+++ record-learning/src/main/java/com/wood/nio/TestSocketNioServer.java
@@ -0,0 +1,100 @@
+package com.wood.nio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestSocketNioServer {
+    private static final Logger log = LoggerFactory.getLogger(TestSocketNioServer.class);
+    private Selector selector;
+    private final static ExecutorService threadPool = Executors.newFixedThreadPool(5);
+
+    public TestSocketNioServer(int serverPort) {
+        this.init(serverPort);
+    }
+
+    private void init(int port) {
+        ServerSocketChannel serverSocketChannel;
+        try {
+            serverSocketChannel = ServerSocketChannel.open();
+            //绑定端口号，此处产生系统调用
+            serverSocketChannel.bind(new InetSocketAddress(port));
+            //设置为非阻塞
+            serverSocketChannel.configureBlocking(false);
+            this.selector = Selector.open();
+            //为通道注册选择器,监听客户端连接事件或者客户端发生错误
+            serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void execute() throws Exception {
+        //select()方法会一直阻塞直到有监听的事件发生
+        while (this.selector.select() > 0) {
+            Iterator<SelectionKey> selectionKeys = this.selector.selectedKeys().iterator();
+            while (selectionKeys.hasNext()) {
+                SelectionKey selectionKey = selectionKeys.next();
+                selectionKeys.remove();
+                if (selectionKey.isAcceptable()) {
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectionKey.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                        log.debug("监听到客户端连接，地址：{}", socketChannel.getRemoteAddress());
+                        //选择器监听客户端发送数据事件
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(this.selector, SelectionKey.OP_READ);
+                } else {
+                    //分配出其他线程来完成客户端发送过来的数据处理
+                    threadPool.execute(new Handler(selectionKey));
+                }
+            }
+        }
+    }
+
+    public static class Handler implements Runnable {
+        private final SelectionKey selectionKey;
+
+        public Handler(SelectionKey selectionKey) {
+            this.selectionKey = selectionKey;
+        }
+
+        @Override
+        public void run() {
+            SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            int len;
+            try {
+                while ((len=socketChannel.read(buffer) )> 0) {
+                    buffer.flip();
+                    log.debug(new String(buffer.array(),0,len));
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                try {
+                    socketChannel.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            new TestSocketNioServer(9090).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- record-learning/src/test/java/com/wood/nio/TestNio.java
+++ record-learning/src/test/java/com/wood/nio/TestNio.java
@@ -6,7 +6,6 @@ import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
